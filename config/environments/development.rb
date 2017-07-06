bb00751b532aba48eb20291811349351e52904fe
Luiz Varela <luiz.lvarela@gmail.com>
2017-07-06 13:54:56 -0300

    Mount Sidekiq admin panel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,13 +12,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
   config.cache_store = :redis_store, ENV['REDIS_URL']
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   get 'disruptions' => 'disruptions#index'
 
   root 'pages#index'
